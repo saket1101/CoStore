c87c1798803f63b4fb1177f3b2ce1886fdbdfc6a
Saket Kumar <saketjha00@gmail.com>
2023-03-28 13:06:44 +0530

    changed confing file

--- config/database.js
+++ config/database.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose')
-// mongoose.set("strictQuery",true)
+mongoose.set("strictQuery",true)
 
 
 const connectDb = async()=>{
+  try{
     const {connection} = await mongoose.connect(process.env.MONGO_URI)
     console.log(`MongoDb connection : ${connection.host}`)
+  }  catch(error){
+    console.log(error.message)
+  }
 }
 
 module.exports= connectDb
